4d1a0e685b02ae1308d954a8241863888ab97241
dermatologist <github@gulfdoctor.net>
2016-07-11 13:42:37 -0400

    Implementations written

--- api/src/main/java/org/openmrs/module/nuform/api/impl/NuformServiceImpl.java
+++ api/src/main/java/org/openmrs/module/nuform/api/impl/NuformServiceImpl.java
@@ -9,11 +9,15 @@
  */
 package org.openmrs.module.nuform.api.impl;
 
-import org.openmrs.api.impl.BaseOpenmrsService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.nuform.Nuform;
+import org.openmrs.module.nuform.NuformDef;
 import org.openmrs.module.nuform.api.NuformService;
 import org.openmrs.module.nuform.api.db.NuformDAO;
+
+import java.util.List;
 
 /**
  * It is a default implementation of {@link NuformService}.
@@ -23,18 +27,64 @@ public class NuformServiceImpl extends BaseOpenmrsService implements NuformServi
 	protected final Log log = LogFactory.getLog(this.getClass());
 	
 	private NuformDAO dao;
-	
+
+    /**
+     * @return the dao
+     */
+    public NuformDAO getDao() {
+        return dao;
+    }
+    
 	/**
      * @param dao the dao to set
      */
     public void setDao(NuformDAO dao) {
 	    this.dao = dao;
     }
-    
-    /**
-     * @return the dao
-     */
-    public NuformDAO getDao() {
-	    return dao;
+
+    @Override
+    public List getAllDefWithDeleted() {
+        return dao.getAllDefWithDeleted();
     }
+
+    @Override
+    public List getAllDef() {
+        return dao.getAllDef();
+    }
+
+    @Override
+    public List getAllNuformsWithDeleted() {
+        return dao.getAllNuformsWithDeleted();
+    }
+
+    @Override
+    public List getAllNuforms() {
+        return dao.getAllNuforms();
+    }
+
+    @Override
+    public List getAllNuformsByDef(NuformDef nuformDef) {
+        return dao.getAllNuformsByDef(nuformDef);
+    }
+
+    @Override
+    public Nuform getNuformById(int id) {
+        return dao.getNuformById(id);
+    }
+
+    @Override
+    public NuformDef getNuformDefById(int id) {
+        return dao.getNuformDefById(id);
+    }
+
+    @Override
+    public Nuform saveNuform(Nuform nuform) {
+        return dao.saveNuform(nuform);
+    }
+
+    @Override
+    public NuformDef saveNuformDef(NuformDef nuformDef) {
+        return dao.saveNuformDef(nuformDef);
+    }
+
 }

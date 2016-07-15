d8d6cfb9def34b18bbeab535192b540d4cc2ccbd
dermatologist <github@gulfdoctor.net>
2016-07-14 22:57:51 -0400

    get completed

--- omod/src/main/java/org/openmrs/module/nuform/page/controller/NuformPageController.java
+++ omod/src/main/java/org/openmrs/module/nuform/page/controller/NuformPageController.java
@@ -1,7 +1,24 @@
 package org.openmrs.module.nuform.page.controller;
 
+import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by beapen on 11/07/16.
  */
 public class NuformPageController {
+
+    public void get(@RequestParam("nuformId") int nuformId,
+                    @RequestParam("nuformDefId") int nuformDefId,
+                    @RequestParam("patientId") int patientId,
+                    @RequestParam("lesionmap") String lesionmap,
+                    @RequestParam("backgroundImage") String backgroundImage,
+                    PageModel model) {
+        model.addAttribute("nuformId", nuformId);
+        model.addAttribute("nuformDefId", nuformDefId);
+        model.addAttribute("patientId", patientId);
+        model.addAttribute("lesionmap", lesionmap);
+        model.addAttribute("backgroundImage", backgroundImage);
+
+    }
 }
